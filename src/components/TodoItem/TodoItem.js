5d8219de0926a4cd5a0b54f599ee0f7fe92a038d
Enrique Villa <enrique.villa97@hotmail.com>
2020-03-17 14:07:28 -0600

    Phase 5 finished

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./TodoItem.css";
 
+let handleClick = () => {
+    console.log("Clicked checkbox")
+}
+
 class TodoItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            complete: this.props.complete
+        }
+    }
     render () {
         return (
             <div className={(this.props.last) && "last"}>
-                <input type="checkbox" checked={this.props.complete}/>
+                <input type="checkbox" checked={this.state.complete} onChange={handleClick}/>
                 <span className="todo-text">
                     {this.props.text}
                 </span>
