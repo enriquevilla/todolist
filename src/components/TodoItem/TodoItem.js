c0f505efdc2aa6ecf77113cd698eecf17e860f44
Enrique Villa <enrique.villa97@hotmail.com>
2020-03-17 13:54:54 -0600

    Removed commented functional component

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,17 +1,6 @@
 import React from "react";
 import "./TodoItem.css";
 
-// let TodoItem = (props) => {
-//     return (
-//         <div className={(props.last) && "last"}>
-//             <input type="checkbox" checked={props.complete}/>
-//             <span className="todo-text">
-//                 {props.text}
-//             </span>
-//         </div>
-//     )
-// }
-
 class TodoItem extends React.Component {
     render () {
         return (
