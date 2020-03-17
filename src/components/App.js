49928d0b29569887fd6eaaeda0d4928b18780805
Enrique Villa <enrique.villa97@hotmail.com>
2020-03-17 13:36:33 -0600

    Phase 4 finished

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,25 @@ import React from "react";
 import TodoItem from "./TodoItem/TodoItem";
 import data from "./TodoItem/TodoData";
 
-let mydata = data.map(i => {
-    if (i === data[data.length - 1]) {
-        i.last = true;
-    }
-    return <TodoItem
-        key={i.id}
-        text={i.text}
-        complete={i.complete}
-        last={i.last}
-    />
-});
-
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: data
+        }
+    }
     render() {
+        const mydata = this.state.todos.map(i => {
+            if (i === data[data.length - 1]) {
+                i.last = true;
+            }
+            return <TodoItem
+                key={i.id}
+                text={i.text}
+                complete={i.complete}
+                last={i.last}
+            />
+        });
         return (
             <div>
                 {mydata}
