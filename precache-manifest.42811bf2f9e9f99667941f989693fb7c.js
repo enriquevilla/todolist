c2b9464cb2cc5bb8ed65f6601bb33758e453174f
Enrique Villa <enrique.villa97@hotmail.com>
2020-03-18 12:47:10 -0600

    Updates

---
+++ precache-manifest.42811bf2f9e9f99667941f989693fb7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670d92076a86ab4b7d180eae3c74798b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ad1025d9649a1f88b7ce",
+    "url": "/todolist/static/css/main.d028da4b.chunk.css"
+  },
+  {
+    "revision": "6be2738800fb263442f6",
+    "url": "/todolist/static/js/2.cab9df89.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todolist/static/js/2.cab9df89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1025d9649a1f88b7ce",
+    "url": "/todolist/static/js/main.4fb308ee.chunk.js"
+  },
+  {
+    "revision": "5dc327340e8e32fc3756",
+    "url": "/todolist/static/js/runtime-main.c337c584.js"
+  }
+]);
