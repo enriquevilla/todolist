58df0ddfda8ab36ddbcdc252fd545b9e3b5e2d17
Enrique Villa <enrique.villa97@hotmail.com>
2020-03-19 15:23:42 -0600

    Updates

--- precache-manifest.902abd8a96b23daf6848c4e90917b790.js
+++ precache-manifest.902abd8a96b23daf6848c4e90917b790.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c74d261a7d3c94e4e03c8472d0454978",
+    "revision": "8669430b0b4a1bb2fe6b5a610f0373db",
     "url": "/todolist/index.html"
   },
   {
-    "revision": "f565c45d8a6767f04545",
+    "revision": "8b50214aefa5bb7af003",
     "url": "/todolist/static/css/main.55c3959a.chunk.css"
   },
   {
-    "revision": "69319071c75c62fdac4d",
-    "url": "/todolist/static/js/2.e61dcb5c.chunk.js"
+    "revision": "cb8d38fe1e4199843334",
+    "url": "/todolist/static/js/2.6ba75c8c.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/todolist/static/js/2.e61dcb5c.chunk.js.LICENSE.txt"
+    "url": "/todolist/static/js/2.6ba75c8c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f565c45d8a6767f04545",
-    "url": "/todolist/static/js/main.7452b07b.chunk.js"
+    "revision": "8b50214aefa5bb7af003",
+    "url": "/todolist/static/js/main.285b9888.chunk.js"
   },
   {
     "revision": "5dc327340e8e32fc3756",
